bb966bc95774971878c8f53b2f72befe02b3e3c2
Kim-chanmi <chanmi1320@gmail.com>
2022-11-08 13:03:42 +0900

    2022.11.08

---
+++ src/components/ContactCont.vue
@@ -0,0 +1,7 @@
+<template>
+  <section className="cont__contact">
+    <p>Let's create something new</p>
+    <h3>LET's Get IN <em>TOUCH</em></h3>
+    <a href="malito:chanmi0517@naver.com">Contact me</a>
+  </section>
+</template>
