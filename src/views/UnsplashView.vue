0f93f2d98b6801a68568b8a0b249907c2e9ea2b3
Kim-chanmi <chanmi1320@gmail.com>
2022-11-15 19:51:32 +0900

    2022.11.15

--- src/views/UnsplashView.vue
+++ src/views/UnsplashView.vue
@@ -2,21 +2,52 @@
   <div>
     <HeaderCont />
     <TitleCont name1="unsplash" name2="reference api" />
-
-    <section class="cont__refer">
+    <section class="unsplash__wrap">
       <div class="container">
         <div class="unsplash__inner">
-          <div class="unsplash__slider"></div>
-          <div class="unsplash__search"></div>
-          <div class="unsplash__images">
-            <ul>
-              <li v-for="splash in splashes" :key="splash.id">
-                <a href="#">
-                  <img :src="splash.urls.regular" :alt="splash.id" />
-                </a>
-              </li>
-            </ul>
-          </div>
+          <h2>Unsplash Random</h2>
+          <ul>
+            <li>
+              <swiper
+                :effect="'cards'"
+                :grabCursor="true"
+                :modules="modules"
+                class="mySwiper"
+              >
+                <swiper-slide v-for="slide in sliders" :key="slide.id">
+                  <a :href="`https://unsplash.com/photos/${slide.id}`">
+                    <img :src="slide.urls.regular" :alt="slide.id" />
+                  </a>
+                </swiper-slide>
+              </swiper>
+            </li>
+          </ul>
+        </div>
+        <div class="btn__list">
+          <button v-for="title in titles" v-bind:key="title.name">
+            {{ title.name }}
+          </button>
+        </div>
+        <div class="unsplash_search">
+          <h2>검색하기</h2>
+          <form @submit.prevent="SearchUnsplashes()">
+            <input
+              type="search"
+              id="search"
+              placeholder="검색하세요"
+              v-model="search"
+            />
+            <button type="submit">검색</button>
+          </form>
+        </div>
+        <div class="cont__unsplash">
+          <ul>
+            <li v-for="splash in splashes" :key="splash.id">
+              <a :href="`https://unsplash.com/photos/${splash.id}`">
+                <img :src="splash.urls.regular" :alt="splash.id" />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
@@ -33,16 +64,39 @@ import TitleCont from "@/components/TitleCont.vue";
 import ContactCont from "@/components/ContactCont.vue";
 import { ref } from "vue";
 
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/effect-cards";
+import { EffectCards } from "swiper";
+
 export default {
   components: {
     HeaderCont,
     FooterCont,
     TitleCont,
     ContactCont,
+    Swiper,
+    SwiperSlide,
+  },
+
+  data: function () {
+    return {
+      modules: [EffectCards],
+      titles: [
+        { name: "3D Renders" },
+        { name: "Experimental" },
+        { name: "Architecture" },
+        { name: "Fashion" },
+        { name: "Nature" },
+        { name: "People" },
+        { name: "Athletics" },
+      ],
+    };
   },
 
   setup() {
     const splashes = ref([]);
+    const sliders = ref([]);
     const search = ref("landscape");
 
     // const SearchSplashes = () => {
@@ -60,40 +114,144 @@ export default {
       )
         .then((response) => response.json())
         .then((result) => (splashes.value = result))
+        // .then((result) => console.log(result))
         .then((error) => console.log(error));
     };
     RandomSplashes();
 
+    const TopSplashes = () => {
+      fetch(
+        "https://api.unsplash.com/photos/random/?client_id=ArGvdKlJWl3YpwcaZmGDf5jEEVo81-0sbA8evCVSBo4&count=10"
+      )
+        .then((response) => response.json())
+        .then((result) => (sliders.value = result))
+        .then((error) => console.log(error));
+    };
+    TopSplashes();
+
+    const SearchUnsplashes = async () => {
+      fetch(
+        `https://api.unsplash.com/search/photos?client_id=ArGvdKlJWl3YpwcaZmGDf5jEEVo81-0sbA8evCVSBo4&query=${search.value}`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          splashes.value = result.results;
+          search.value = "";
+        })
+        .catch((error) => console.log(error));
+    };
+    SearchUnsplashes();
+
     return {
       splashes,
       search,
-      // SearchSplashes,
+      sliders,
       RandomSplashes,
+      TopSplashes,
+      SearchUnsplashes,
     };
   },
 };
 </script>
 <style lang="scss">
-.unsplash__images {
+body {
+  overflow-x: hidden;
+}
+.cont__unsplash {
   ul {
-    list-style: none;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    gap: 30px;
-    li {
-      width: 23%;
-      margin-bottom: 70px;
-      transition: all 0.3s;
+    column-count: 4;
+    column-gap: 20px;
+    width: 100%;
 
-      &:hover {
-        transform: scale3d(1.05);
-      }
-      a {
-        img {
-        }
+    li {
+      margin-bottom: 20px;
+
+      img {
+        border-radius: 5px;
       }
     }
+  }
+}
+
+.unsplash__inner {
+  margin-bottom: 70px;
+  h2 {
+    font-size: 40px;
+    color: var(--black);
+    margin-bottom: 70px;
+  }
+  .swiper-slide img {
+    width: 250px !important;
+    height: 300px !important;
+  }
+
+  .swiper-slide {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 18px;
+    font-size: 22px;
+    font-weight: bold;
+    color: #fff;
+  }
+
+  a {
+    color: var(--white);
+  }
+}
+.unsplash_search {
+  margin-bottom: 100px;
+  position: relative;
+
+  h2 {
+    color: var(--black);
+    font-size: 40px;
+    text-indent: -9999px;
+    height: 0;
+  }
+
+  input {
+    background: #d5d5d5;
+    border: 1px solid #d5d5d5;
+    border-radius: 50px;
+    color: var(--black);
+    width: 100%;
+    padding: 14px 30px;
+    font-family: var(--font-kor1);
+  }
+  button {
+    position: absolute;
+    right: 8px;
+    top: 5px;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    border: 0;
+    font-family: var(--font-kor1);
+    cursor: pointer;
+    z-index: 1000;
+    color: #000;
+  }
+}
+.btn__list {
+  margin-top: -40px;
+  margin-bottom: 60px;
+  text-align: center;
+
+  button {
+    display: inline-block;
+    padding: 10px;
+    margin-right: 10px;
+    border-radius: 20px;
+    cursor: pointer;
+    background: #000;
+    color: #fff;
+    border: 1px solid #fff;
+  }
+  button:hover {
+    background: #fff;
+    color: #000;
+    transition: all 0.3s;
   }
 }
 </style>
--- src/views/YoutubeView.vue
+++ src/views/YoutubeView.vue
@@ -2,6 +2,73 @@
   <div>
     <HeaderCont />
     <TitleCont name1="youtube" name2="reference api" />
+    <section class="youtube__wrap">
+      <div class="container">
+        <div class="youtube__popular">
+          <div class="youtube__inner">
+            <h2>Youtube Random</h2>
+            <li>
+              <swiper
+                :grabCursor="true"
+                :effect="'creative'"
+                :creativeEffect="{
+                  prev: {
+                    shadow: false,
+                    translate: [0, 0, -400],
+                  },
+                  next: {
+                    translate: ['100%', 0, 0],
+                  },
+                }"
+                :modules="modules"
+                class="mySwiper"
+              >
+                <swiper-slide
+                  v-for="slider in sliders"
+                  :key="slider.id.videoId"
+                >
+                  <a
+                    :href="`https://www.youtube.com/watch?v=${slider.id.videoId}`"
+                  >
+                    <img
+                      :src="slider.snippet.thumbnails.medium.url"
+                      :alt="slider.snippet.description"
+                    />
+                  </a>
+                </swiper-slide>
+              </swiper>
+            </li>
+          </div>
+        </div>
+        <div class="movie__search">
+          <h2>검색하기</h2>
+          <form @submit.prevent="SearchYoutubes()">
+            <input
+              type="search"
+              id="search"
+              placeholder="검색하세요"
+              v-model="search"
+            />
+            <button type="submit">검색</button>
+          </form>
+        </div>
+        <div class="youtube__inner">
+          <ul>
+            <li v-for="youtube in youtubes" :key="youtube.id.videoId">
+              <a
+                :href="`https://www.youtube.com/watch?v=${youtube.id.videoId}`"
+              >
+                <img
+                  :src="youtube.snippet.thumbnails.medium.url"
+                  :alt="youtube.snippet.description"
+                />
+                <span>{{ youtube.snippet.title }}</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
     <ContactCont />
     <FooterCont />
   </div>
@@ -12,6 +79,12 @@ import HeaderCont from "@/components/HeaderCont.vue";
 import FooterCont from "@/components/FooterCont.vue";
 import TitleCont from "@/components/TitleCont.vue";
 import ContactCont from "@/components/ContactCont.vue";
+import { ref } from "vue";
+
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/effect-creative";
+import { EffectCreative } from "swiper";
 
 export default {
   components: {
@@ -19,6 +92,122 @@ export default {
     FooterCont,
     TitleCont,
     ContactCont,
+    Swiper,
+    SwiperSlide,
+  },
+
+  setup() {
+    const youtubes = ref([]);
+    const sliders = ref([]);
+    const search = ref("landscape");
+
+    const RandomYoutube = () => {
+      fetch(
+        "https://raw.githubusercontent.com/Kim-chanmi/react_api/main/src/utils/youtube_video.json"
+      )
+        .then((response) => response.json())
+        .then((result) => (youtubes.value = result.items))
+        // .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
+    };
+    RandomYoutube();
+
+    const TopYoutube = () => {
+      fetch(
+        "https://raw.githubusercontent.com/Kim-chanmi/react_api/main/src/utils/youtube_video.json"
+      )
+        .then((response) => response.json())
+        .then((result) => (sliders.value = result.items))
+        // .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
+    };
+    TopYoutube();
+
+    const SearchYoutubes = () => {
+      fetch(
+        `https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyBMw5RG0lHjQd8xTnmA63HCsEe5tXuQ764&part=snippet&maxResults=10&type=video&q=${search.value}`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          youtubes.value = result.items;
+          search.value = "";
+        })
+        // .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
+    };
+    SearchYoutubes();
+
+    return {
+      modules: [EffectCreative],
+      youtubes,
+      sliders,
+      search,
+      RandomYoutube,
+      TopYoutube,
+      SearchYoutubes,
+    };
   },
 };
 </script>
+<style lang="scss">
+.youtube__inner {
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    font-family: var(--font-kor1);
+    li {
+      width: 24%;
+      margin-bottom: 20px;
+
+      span {
+        display: inline-block;
+        padding: 5px 0;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        width: 100%;
+      }
+      img {
+        border-radius: 5px;
+        overflow: hidden;
+      }
+    }
+  }
+}
+.youtube__inner {
+  h2 {
+    font-size: 40px;
+    color: var(--black);
+  }
+  li {
+    list-style: none;
+  }
+  .swiper img {
+    margin: 100px auto;
+  }
+
+  .swiper-slide {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 22px;
+    font-weight: bold;
+  }
+}
+.youtube__popular {
+  ul {
+    column-count: 4;
+    column-gap: 20px;
+    width: 100%;
+
+    li {
+      margin-bottom: 20px;
+
+      img {
+        border-radius: 5px;
+      }
+    }
+  }
+}
+</style>
